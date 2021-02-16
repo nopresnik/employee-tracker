a648039ff03054796375159c4bbeea759f7ba230
Nathan Opresnik <nopresnik@me.com>
2021-02-16 22:06:21 +1100

    Make database testable

--- src/db/database.js
+++ src/db/database.js
@@ -3,14 +3,24 @@ const mysql = require("mysql");
 let pool;
 
 module.exports = {
-  getPool: () => {
+  getPool: (test = false) => {
     if (pool) return pool;
-    pool = mysql.createPool({
-      host: settings.DB_HOST,
-      user: settings.DB_USER,
-      password: settings.DB_PASS,
-      database: settings.DB_NAME,
-    });
+
+    let connectionProps = !test
+      ? {
+          host: settings.DB_HOST,
+          user: settings.DB_USER,
+          password: settings.DB_PASS,
+          database: settings.DB_NAME,
+        }
+      : {
+          host: settings.DB_TEST_HOST,
+          user: settings.DB_TEST_USER,
+          password: settings.DB_TEST_PASS,
+          database: settings.DB_TEST_NAME,
+        };
+
+    pool = mysql.createPool(connectionProps);
     return pool;
   },
 };
