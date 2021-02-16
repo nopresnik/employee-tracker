4e539d668add5eb35162ee24532f149344715f0d
Nathan Opresnik <nopresnik@me.com>
2021-02-17 01:16:16 +1100

    Use async await

--- src/controllers/roles.js
+++ src/controllers/roles.js
@@ -1,11 +1,9 @@
 const inquirer = require("inquirer");
 const services = require("../services");
 const prompts = require("../prompts");
-const { prompt } = require("inquirer");
-const { getDepByName } = require("../services/departments");
 
-const showAllRoles = (cb) => {
-  services.roles.getAllRoles((result) => {
+const showAllRoles = async (cb) => {
+  services.roles.getAllRoles().then((result) => {
     console.clear();
     console.table(result);
     cb();
@@ -13,25 +11,6 @@ const showAllRoles = (cb) => {
 };
 
 const addNewRole = async (cb) => {
-  // services.departments.getAllDepartments((depsres) => {
-  //   let departments = depsres.map((dep) => dep.name);
-
-  //   inquirer.prompt(prompts.getAddRolePrompts(departments)).then((answer) => {
-  //     const { roleTitle, roleSalary, roleDepartment } = answer;
-  //     services.departments.getDepByName(roleDepartment, (department) => {
-  //       services.roles.createRole(
-  //         {
-  //           title: roleTitle,
-  //           salary: roleSalary,
-  //           department: departme.id,
-  //         },
-  //         () => {
-  //           cb();
-  //         }
-  //       );
-  //     });
-  //   });
-  // });
   let departments = await services.departments.getAllDepartments();
   departments = departments.map((dep) => dep.name);
 
