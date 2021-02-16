9d3a587a2b7e115fd8a36bdd6429e3c507b9987a
Nathan Opresnik <nopresnik@me.com>
2021-02-16 20:25:00 +1100

    Fix create role

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const app = require("./src/app");
 // app.start();
 
 const services = require("./src/services");
-services.roles.getAllRoles((res) => console.log(res));
+
+const newRole = {
+  title: "Test role",
+  salary: 50000,
+  department: 1,
+};
+
+services.roles.createRole(newRole, (res) => console.log(res));
--- src/services/roles.js
+++ src/services/roles.js
@@ -17,7 +17,7 @@ const createRole = (role, done) => {
   pool.getConnection((err, connection) => {
     if (err) throw err;
     connection.query(
-      "INSERT INTO role (role) VALUES (??)",
+      "INSERT INTO role (title, salary, department_id) VALUES (?,?,?)",
       [title, salary, department],
       (err, res) => {
         if (err) throw err;
