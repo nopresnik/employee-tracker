eb8ed2bcc58c074968fc9f91a09d5d1f64710b74
Nathan Opresnik <nopresnik@me.com>
2021-02-16 16:25:55 +1100

    Create menu prompts

--- src/prompts/main.js
+++ src/prompts/main.js
@@ -1,1 +1,31 @@
-module.exports = [{}];
+const constants = {
+  add_dep: "Add department",
+  add_role: "Add new role",
+  add_emp: "Add new employee",
+  view_dep: "View departments",
+  view_roles: "View roles",
+  view_emp: "View employees",
+  update_emp: "Update employee",
+};
+
+const prompts = [
+  {
+    type: "list",
+    name: "action",
+    message: "What would you like to do?",
+    choices: [
+      constants.add_dep,
+      constants.add_role,
+      constants.add_emp,
+      constants.view_dep,
+      constants.view_roles,
+      constants.view_emp,
+      constants.update_emp,
+    ],
+  },
+];
+
+module.exports = {
+  constants,
+  prompts,
+};
