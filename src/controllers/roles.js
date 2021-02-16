355f47868fca3c8cae7d4ce35e130bce7ae0534f
Nathan Opresnik <nopresnik@me.com>
2021-02-17 01:12:25 +1100

    Use async await

--- src/controllers/roles.js
+++ src/controllers/roles.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const services = require("../services");
 const prompts = require("../prompts");
+const { prompt } = require("inquirer");
+const { getDepByName } = require("../services/departments");
 
 const showAllRoles = (cb) => {
   services.roles.getAllRoles((result) => {
@@ -11,25 +13,43 @@ const showAllRoles = (cb) => {
 };
 
 const addNewRole = async (cb) => {
-  services.departments.getAllDepartments((depsres) => {
-    let departments = depsres.map((dep) => dep.name);
+  // services.departments.getAllDepartments((depsres) => {
+  //   let departments = depsres.map((dep) => dep.name);
 
-    inquirer.prompt(prompts.getAddRolePrompts(departments)).then((answer) => {
+  //   inquirer.prompt(prompts.getAddRolePrompts(departments)).then((answer) => {
+  //     const { roleTitle, roleSalary, roleDepartment } = answer;
+  //     services.departments.getDepByName(roleDepartment, (department) => {
+  //       services.roles.createRole(
+  //         {
+  //           title: roleTitle,
+  //           salary: roleSalary,
+  //           department: departme.id,
+  //         },
+  //         () => {
+  //           cb();
+  //         }
+  //       );
+  //     });
+  //   });
+  // });
+  let departments = await services.departments.getAllDepartments();
+  departments = departments.map((dep) => dep.name);
+
+  inquirer
+    .prompt(prompts.getAddRolePrompts(departments))
+    .then(async (answer) => {
       const { roleTitle, roleSalary, roleDepartment } = answer;
-      services.departments.getDepByName(roleDepartment, (department) => {
-        services.roles.createRole(
-          {
-            title: roleTitle,
-            salary: roleSalary,
-            department: department.id,
-          },
-          () => {
-            cb();
-          }
-        );
-      });
+      const department = await services.departments.getDepByName(
+        roleDepartment
+      );
+      services.roles
+        .createRole({
+          title: roleTitle,
+          salary: roleSalary,
+          department: department.id,
+        })
+        .then(() => cb());
     });
-  });
 };
 
 module.exports = { showAllRoles, addNewRole };
