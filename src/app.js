b25a37608d8bb2d6f88b7fd44167994d00e74c5b
Nathan Opresnik <nopresnik@me.com>
2021-02-16 20:27:43 +1100

    Remove old code comments

--- src/app.js
+++ src/app.js
@@ -1,13 +1,3 @@
-// const db = require("./db/database");
-// const pool = db.getPool();
-
-// pool.getConnection((err, connection) => {
-//   connection.query("SELECT * FROM employee", (err, res) => {
-//     if (err) throw err;
-//     console.log(res);
-//   });
-// });
-
 const inquirer = require("inquirer");
 const prompts = require("./prompts");
 
